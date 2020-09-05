28fb02f3233353a1fec40d3d9c53b90f810c5687
Renata-marten <ren_ish@mail.ru>
2020-09-05 14:26:58 +0500

    18 модуль финал

---
+++ Task 18.1.py
@@ -0,0 +1,18 @@
+s = input('Введите два числа через запятую\n')
+l = len(s)
+list = []
+i = 0
+while i < l:
+    s_int = ''
+    a = s[i]
+    while '0' <= a <= '9':
+        s_int += a
+        i += 1
+        if i < l:
+            a = s[i]
+        else:
+            break
+    i += 1
+    if s_int != '':
+        list.append(int(s_int))
+print(list)
